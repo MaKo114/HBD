a26aa3dde1b35f6bc12997a7b74cf819d6ab29ae
MaKo114 <67015114@kmitl.ac.th>
2025-04-30 01:15:37 +0700

    demo1
    almost success

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
-const sections = document.querySelectorAll('section');
+function checkPassword() {
+  const c1 = document.getElementById("c1").value.trim().toLowerCase();
+  const c2 = document.getElementById("c2").value.trim().toLowerCase();
+  const c3 = document.getElementById("c3").value.trim().toLowerCase();
+  const c4 = document.getElementById("c4").value.trim().toLowerCase();
+  const c5 = document.getElementById("c5").value.trim().toLowerCase();
+  const c6 = document.getElementById("c6").value.trim().toLowerCase();  
+  const correct = "tonfon"; // เช่น minnie หรือ mint
 
-window.addEventListener('scroll', () => {
-    sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top < window.innerHeight - 100) {
-            section.style.opacity = 1;
-            section.style.transform = 'translateY(0)';
-        }
-    });
+  const sumChar = c1 + c2 + c3 + c4 + c5 + c6
+
+  if (sumChar === correct) {
+    
+    const textdisplay = document.getElementById("hbdtext");
+    const endtext = document.getElementById('showtext')
+    const picturedisplay = document.getElementById("hiddenpicuture")
+    const body = document.querySelector('body')
+    body.style.height = '300vh'
+
+    textdisplay.style.display = "block";
+    textdisplay.style.animation = 'fadeInUp 2s ease-in-out';
+    picturedisplay.style.display = "block";
+    picturedisplay.style.animation = 'fadeInUp 2s ease-in-out';
+    
+    endtext.scrollIntoView({ behavior: "smooth", block: 'start' })
+    
+
+
+  } else {
+    alert("ลองอีกครั้งน้า~");
+  }
+}
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "cards",
+  grabCursor: true,
+});
+
+
+const boxes = document.querySelectorAll(".charip");
+
+boxes.forEach((box, index) => {
+  // Event listener สำหรับการกรอกข้อความ
+  box.addEventListener("input", () => {
+    if (box.value.length === 1 && index < boxes.length - 1) {
+      boxes[index + 1].focus();
+    }
+  });
+
+  // Event listener สำหรับการกด Backspace
+  box.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && box.value === "" && index > 0) {
+      boxes[index - 1].focus();
+    }
+  });
 });

62d6968e4cde717b5e859ad30784600e7f9df655
MaKo114 <67015114@kmitl.ac.th>
2025-05-01 01:25:42 +0700

    demo 1.0
    almost done

--- script.js
+++ script.js
@@ -4,28 +4,30 @@ function checkPassword() {
   const c3 = document.getElementById("c3").value.trim().toLowerCase();
   const c4 = document.getElementById("c4").value.trim().toLowerCase();
   const c5 = document.getElementById("c5").value.trim().toLowerCase();
-  const c6 = document.getElementById("c6").value.trim().toLowerCase();  
+  const c6 = document.getElementById("c6").value.trim().toLowerCase();
   const correct = "tonfon"; // เช่น minnie หรือ mint
 
-  const sumChar = c1 + c2 + c3 + c4 + c5 + c6
+  const sumChar = c1 + c2 + c3 + c4 + c5 + c6;
 
   if (sumChar === correct) {
-    
     const textdisplay = document.getElementById("hbdtext");
-    const endtext = document.getElementById('showtext')
-    const picturedisplay = document.getElementById("hiddenpicuture")
-    const body = document.querySelector('body')
-    body.style.height = '300vh'
+    const endtext = document.getElementById("showtext");
+    const picturedisplay = document.getElementById("polarnorth");
+    const body = document.querySelector("body");
+    body.style.height = "300vh";
 
     textdisplay.style.display = "block";
-    textdisplay.style.animation = 'fadeInUp 2s ease-in-out';
-    picturedisplay.style.display = "block";
-    picturedisplay.style.animation = 'fadeInUp 2s ease-in-out';
-    
-    endtext.scrollIntoView({ behavior: "smooth", block: 'start' })
-    
+    textdisplay.style.animation = "fadeInUp 2s ease-in-out";
+    picturedisplay.style.display = "grid";
+    picturedisplay.style.justifyContent = "center";
+    picturedisplay.style.alignItems = "center";
+    picturedisplay.style.placeItems = "center";
+    picturedisplay.style.gridTemplateColumns = "repeat(2, 1fr)";
+    picturedisplay.style.gridTemplateRows = "repeat(2, auto)";
+    picturedisplay.style.gap = "10px";
+    picturedisplay.style.animation = "fadeInUp 2s ease-in-out";
 
-
+    endtext.scrollIntoView({ behavior: "smooth", block: "start" });
   } else {
     alert("ลองอีกครั้งน้า~");
   }
@@ -35,7 +37,6 @@ var swiper = new Swiper(".mySwiper", {
   effect: "cards",
   grabCursor: true,
 });
-
 
 const boxes = document.querySelectorAll(".charip");
 
@@ -54,3 +55,14 @@ boxes.forEach((box, index) => {
     }
   });
 });
+window.onload = function () {
+  setTimeout(() => {
+      document.body.classList.add("fade-in");
+  }, 500);
+};
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+  setTimeout(() => {
+      window.location.reload(); // รีเฟรชหลังจาก ScrollTo ด้านบน
+  }, 100);
+};
